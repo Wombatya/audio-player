1ea5f1db291cabfe870ef24a808c9f4259503414
Nadezhda Komarova <naduha.9612@gmail.com>
2023-09-24 17:06:45 +0300

    feat: add visual

---
+++ index.js
@@ -0,0 +1,6 @@
+const container = document.querySelector('.player-container');
+const background = document.querySelectorAll('.background');
+const playerPicture = document.querySelector('.player-picture');
+const songInfo = document.querySelectorAll('.song-info');
+const prevSong = document.getElementById('prev-song');
+const nextSong = document.getElementById('next-song');
